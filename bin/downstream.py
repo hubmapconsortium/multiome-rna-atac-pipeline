e7f54279e3eac606f749c7c6fb004ee06ce7268b
Sarah Schultz <sschultz@vm010.hive.psc.edu>
2023-12-18 15:18:22 -0500

    Update downstream analysis for combined atac/rna mofa, add plot_utils.py

--- bin/downstream.py
+++ bin/downstream.py
@@ -16,17 +16,13 @@ def main(muon_dir: Path):
     rna_expr = expr["rna"]
     rna_expr.X = rna_expr.layers["spliced"]
     print(rna_expr)
-    adt_expr = expr["adt"]
-    print("Construct muon object with RNA and ADT expression data.")
-    mdata_raw = mu.MuData({"rna": rna_expr, "adt": adt_expr})
+    atac_cbg_expr = expr["atac_cell_by_gene"]
+    print("Construct muon object with RNA and ATAC cell-by-gene data.")
+    mdata_raw = mu.MuData({"rna": rna_expr, "atac_cbg": atac_cbg_expr})
     print(mdata_raw)
 
     mdata_rna = mdata_raw.mod["rna"]
-    mdata_adt = mdata_raw.mod["adt"]
-
-    ## ADT normalization: CLR method
-    print("Normalizing ADT data...")
-    pt.pp.clr(mdata_adt)
+    mdata_ataccbg = mdata_raw.mod["atac_cbg"]
 
     ## RNA normalization
     # Can filter out cells that do not pass QC
@@ -34,24 +30,27 @@ def main(muon_dir: Path):
     sc.pp.normalize_total(mdata_rna, target_sum=1e4)
     sc.pp.log1p(mdata_rna)
 
-    ## Downstream analysis for ADT
-    print("Performing downstream analysis for ADT...")
-    sc.tl.pca(mdata_adt)
-    sc.pp.neighbors(mdata_adt)
+    #ATAC TFIDF normalization
+    mu.atac.pp.tfidf(mdata_ataccbg, scale_factor=1e4)
+    
+    ## Downstream analysis for Atac
+    print("Performing downstream analysis for ATAC...")
+    sc.tl.pca(mdata_ataccbg)
+    sc.pp.neighbors(mdata_ataccbg)
 
     for axis in [0, 1]:
-        neighbor_counts = np.array((mdata_adt.obsp["distances"] > 0).sum(axis=axis)).flatten()
-        mdata_adt.obs.loc[:, f"neighbor_counts_ax{axis}"] = neighbor_counts
+        neighbor_counts = np.array((mdata_ataccbg.obsp["distances"] > 0).sum(axis=axis)).flatten()
+        mdata_ataccbg.obs.loc[:, f"neighbor_counts_ax{axis}"] = neighbor_counts
         cells_with_neighbors = (neighbor_counts > 0).astype(int)
-        mdata_adt.obs.loc[:, f"has_neighbors_ax{axis}"] = cells_with_neighbors
-    mdata_adt.obs.loc[:, "has_neighbors"] = mdata_adt.obs.loc[:, "has_neighbors_ax1"]
-    mu.pp.filter_obs(mdata_adt, "has_neighbors", lambda x: x > 0)
+        mdata_ataccbg.obs.loc[:, f"has_neighbors_ax{axis}"] = cells_with_neighbors
+    mdata_ataccbg.obs.loc[:, "has_neighbors"] = mdata_adt.obs.loc[:, "has_neighbors_ax1"]
+    mu.pp.filter_obs(mdata_ataccbg, "has_neighbors", lambda x: x > 0)
 
-    sc.tl.leiden(mdata_adt)
-    sc.tl.umap(mdata_adt)
+    sc.tl.leiden(mdata_ataccbg)
+    sc.tl.umap(mdata_ataccbg)
     with new_plot():
-        sc.pl.umap(mdata_adt, color="leiden", legend_loc="on data")
-        plt.savefig("leiden_cluster_adt.pdf")
+        sc.pl.umap(mdata_ataccbg, color="leiden", legend_loc="on data")
+        plt.savefig("leiden_cluster_atac.pdf")
 
     ## Downstream analysis for RNA
     print("Performing downstream analysis for RNA...")
@@ -95,13 +94,13 @@ def main(muon_dir: Path):
     print(mdata_raw)
     mdata_raw.write("citeseq_normalized.h5mu")
     ## Multi-omics factor analysis
-    mdata_adt.var["highly_variable"] = True
+    mdata_ataccbg.var["highly_variable"] = True
     mdata_raw.update()
-    mu.tl.mofa(mdata_raw, outfile="citeseq_mofa.hdf5", n_factors=30)
+    mu.tl.mofa(mdata_raw, outfile="multiome_mofa.hdf5", n_factors=30)
 
     # multiplex clustering
     sc.pp.neighbors(mdata_raw["rna"])
-    sc.pp.neighbors(mdata_raw["adt"])
+    sc.pp.neighbors(mdata_raw["atac_cbg"])
     print(mdata_raw)
     sc.pp.neighbors(mdata_raw, use_rep="X_mofa", key_added="mofa")
     sc.tl.umap(mdata_raw, neighbors_key="mofa")
@@ -111,7 +110,7 @@ def main(muon_dir: Path):
         sc.pl.umap(mdata_raw, color="leiden_wnn", legend_loc="on data")
         plt.savefig("leiden_cluster_combined.pdf")
 
-    mdata_raw.write("citeseq_downstream.h5mu")
+    mdata_raw.write("multiome_downstream.h5mu")
 
 
 if __name__ == "__main__":
